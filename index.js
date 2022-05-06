3c6f16c3501171832bfc7a7c7cf837069054f1e7
zubayer-salehin <zubaersalehin@gmail.com>
2022-05-06 18:51:43 +0600

    pagination count get

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const cors = require("cors")
+// const cors = require("cors")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
